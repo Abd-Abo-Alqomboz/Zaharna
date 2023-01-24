b24fc6ed6d38eb5abc3c85ea91e96bcef593a6ec
Abd-Abo-Alqomboz <80759403+Abd-Abo-Alqomboz@users.noreply.github.com>
2023-01-24 09:30:02 +0200

    abd

---
+++ موقع الزهارنة/assets/js/main.js
@@ -0,0 +1,10 @@
+/* start float action buuton */
+document.querySelector('.fab').addEventListener('click',function(e){
+    document.querySelector('.box').classList.toggle('box-active');
+    document.querySelector('.fab').classList.toggle('fab-active');
+    });
+/* end float action buuton */
+/* START VR */
+
+/* END VR */
+
